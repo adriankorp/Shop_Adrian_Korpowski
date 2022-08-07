af62138cfe9738bcd8c71e5c5699c54052313b75
adriankorp <adrian.korpowski@gmail.com>
2022-08-07 17:39:16 +0200

    Working navbar, currency switch, categories

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import { Switch, Route, Link, BrowserRouter as Router } from "react-router-dom";
+import { client } from "./index";
+import "./App.css";
+import { Component } from "react";
+import Context from "./Context";
+import {
+  LOAD_ALL_PRODUCTS,
+  LOAD_CATEGOTY,
+  LOAD_PRODUCT,
+  LOAD_PRODUCTS_OF_CATEGOTY,
+} from "./GraphQL/Queries";
+import ProductCard from "./components/ProductCard/ProductCard";
+import Navbar from "./components/Navbar/Navbar";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      cart: {},
+      products: [],
+      selectedCurrency:'$'
+    };
+  }
+
+  changeCurrency = currencySymbol => {
+    this.setState({selectedCurrency: currencySymbol})
+  }
+
+  loadCategory = category => {
+    client.query({
+      query: LOAD_PRODUCTS_OF_CATEGOTY,
+      variables: { title: category },
+    }).then((res) => this.setState({products: [...res.data.category.products]}))
+
+  }
+
+
+  componentDidMount() {
+    }
+
+  render() {
+    return (
+      <Context.Provider
+        value={{
+          ...this.state,
+          changeCurrency: this.changeCurrency,
+          loadCategory: this.loadCategory,
+          removeFromCart: this.removeFromCart,
+          addToCart: this.addToCart,
+          login: this.login,
+          addProduct: this.addProduct,
+          clearCart: this.clearCart,
+          checkout: this.checkout,
+        }}
+      >
+        <>
+          <Navbar></Navbar>
+          <div className="product-list">
+            {this.state.products.map((el)=>{
+              return <ProductCard key={el.id} {...el}/>
+            })}
+          </div>
+        </>
+      </Context.Provider>
+    );
+  }
 }
-
-export default App;
